ca54bfc471cb76e7c90651a1e2c4efc8342bca86
Sam Urmy <eloceanografo@gmail.com>
2014-11-26 20:48:23 -0500

    finish function for reading data, func to link all plausible target paths

--- src/MCMCDA.jl
+++ src/MCMCDA.jl
@@ -4,7 +4,10 @@ using Distributions, DataFrames, Graphs, StateSpace
 
 export Blip, 
 	ScanGraph, 
-	read_targets
+	read_targets,
+	link!
+
+#################################################
 
 type Blip{T}
 	x::Vector{T}
@@ -12,8 +15,16 @@ type Blip{T}
 end
 Blip{T1, T2}(x::Array{T1, 1}, t::T2) = Blip(x, convert(T1, t))
 
-type ScanGraph{T}
-	scans::Vector{Vector{Blip{T}}}
+function distance{T}(b1::Blip{T}, b2::Blip{T})
+	return sqrt(sum((b1.x - b2.x).^2))
+end
+
+function get_blip(v::ExVertex)
+	return v.attributes["blip"]
+end
+
+type ScanGraph
+	scans::Vector{Vector{ExVertex}}
 	graph::AbstractGraph
 	nblips::Vector{Int64}
 	nscans::Int64
@@ -21,13 +32,51 @@ end
 
 function read_targets(filename)
 	blip_table = readtable("data/test_targets.csv")
-	scans = Vector{Blip}[]
+	scans = Vector{ExVertex}[]
+	g = graph(ExVertex[], ExEdge{ExVertex}[], is_directed=true)
+	vertex_i = 1
+	# make array of arrays of blips
 	for df in groupby(blip_table, :t)
-		blips = [Blip(vec(array(df[i, [:x, :y]])), df[i, :t])
-					for i in 1:size(df, 1)]
-		push!(scans, blips)
+		this_scan = ExVertex[]
+		for i in 1:size(df, 1)
+			b = Blip(vec(array(df[i, [:x, :y]])), df[i, :t])
+			v = ExVertex(vertex_i, string(vertex_i))
+			v.attributes["blip"] = b
+			push!(this_scan, v)
+			add_vertex!(g, v)
+			vertex_i += 1
+		end
+		push!(scans, this_scan)
 	end
-	return scans
+	nblips = [length(scan) for scan in scans]
+	nscans = length(scans)
+	return ScanGraph(scans, g, nblips, nscans)
+end
+
+
+# max_missed = floor(log(1 - p_max_lost) / log(p_detect))
+# max_missed = max(max_missed, 1)
+# max_missed = min(g.nscans - 1, max_missed)
+
+
+function link!(g::ScanGraph, max_dist, max_missed)
+	edge_i = 1
+	for i in 1:(g.nscans - max_missed)
+		for j in (i + 1):(i + max_missed)
+			for m in 1:length(g.scans[i])
+				for n in 1:length(g.scans[j])
+					v1 = g.scans[i][m]
+					v2 = g.scans[j][n]
+					if distance(get_blip(v1), get_blip(v2)) < max_dist
+						e = ExEdge(edge_i, v1, v2)
+						e.attributes["active"] = false
+						add_edge!(g.graph, e)
+						edge_i += 1
+					end
+				end
+			end
+		end
+	end
 end
 
 end # module
--- test/test_io.jl
+++ test/test_io.jl
@@ -1,5 +1,9 @@
 
 println("Running test_io.jl")
 
+println("Loading data...")
 blips = read_targets("data/test_targets.csv")
 
+println("Linking graph...")
+link!(blips, 25, 2)
+println(blips.graph)
