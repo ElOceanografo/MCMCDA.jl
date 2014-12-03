0b9688a54a181185d8effd0b3b0585fddfd6c37a
Sam Urmy <eloceanografo@gmail.com>
2014-12-02 23:50:46 -0500

    change io test to reflect renaming link! -> connect_graph!

--- test/test_io.jl
+++ test/test_io.jl
@@ -4,9 +4,9 @@ println("Running test_io.jl")
 println("Loading data...")
 blips = read_targets("data/test_targets.csv")
 
-println("Linking graph...")
-link!(blips, 25, 2)
+println("Connecting graph...")
+connect_graph!(blips, 25, 2)
 println(blips)
 
-@time link!(blips, 25, 2)
+@time connect_graph!(blips, 25, 2)
 println(blips)
