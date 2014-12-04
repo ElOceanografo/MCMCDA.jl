22b77a7f1df45240b296fd228542311fe8674436
Sam Urmy <eloceanografo@gmail.com>
2014-12-03 22:57:16 -0500

    implement calculation of configuration prior
    also, correct definition of lambda in likelihood: false detections per scan now, instead of total

--- src/probabilities.jl
+++ src/probabilities.jl
@@ -19,11 +19,41 @@ end
 function loglikelihood(sg::ScanGraph, lambda::Float64, 
 		track_model::LinearGaussianSSM)
 	LL = 0
-	LL += logpdf(Poisson(lambda), n_false_targets(sg))
+	LL += logpdf(Poisson(lambda * sg.nscans), n_false_targets(sg))
 	for v in vertices(sg.graph)
 		if starts_track(v, sg)
 			LL += track_loglikelihood(v, sg, track_model)
 		end
 	end
 	return LL
+end
+
+function summary_numbers(sg::ScanGraph, i::Integer)
+	zt = n_tracks_ended(sg, i)
+	nt = n_tracks(sg, i) # total tracks present
+	at = n_tracks_started(sg, i) # new ones this step
+	ct = nt - zt - at # tracks continuing from last sweep
+	dt = n_targets(sg, i) # total detected
+	gt = nt - dt # number of undetected targets
+	ft = n_false_targets(sg, i)
+	return (zt, ct, dt, gt, at, ft)
+end
+
+function config_logprior(sg::ScanGraph, params::Vector{Float64}, vol::Float64, 
+		t1::Integer, t2::Integer)
+	# parameters
+	pz, pd, lambda_b, lambda_f = params
+	@assert (pz > 0) && (pd > 0) "Probabilities must be > 0"
+	@assert (pz < 1) && (pd < 1) "Probabilities must be < 1"
+
+	LP = 0
+	for i in t1:t2
+		zt, ct, dt, gt, at, ft = summary_numbers(sg, i)
+		# add log probabilities for each piece of the prior (Oh et al eqn. 9)
+		LP += zt * log(pz) + ct * log(1 - pz)
+		LP += dt * log(pd) +  gt * log(1 - pd)
+		LP += at * log(lambda_b * vol)
+		LP += ft * log(lambda_f * vol)
+	end
+	return LP
 end
